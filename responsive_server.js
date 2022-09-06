d4556f0348242c05e1186dd60ce46ac8a15a54dc
Anonymous <anonymous@person.com>
2022-09-06 13:44:37 +0100

    Updated server with a responsive HTML page- currently has a dummy toUpperCase() function instead of future ELIZA responses.

--- responsive_server.js
+++ responsive_server.js
@@ -1,11 +1,26 @@
+
 var http = require('http');
-var dt = require('./date_module');
-var eliza_says = require('./eliza_speech');
+var fs = require('fs');
+var events = require('events');
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    //res.write('Hello, I am Eliza.\n');
-    res.write(eliza_says.eliza_greeting());
-    res.write('\r\nYou spoke to me at ' + dt.myDateTime() + '.');
-    res.end();
+    fs.readFile('demofile1.html', function(err, data){
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write(data);
+        return res.end();
+    });
+
 }).listen(8080);
+
+var eventEmitter = new events.EventEmitter();
+
+//Create an Event Handler
+var myEventHandler = function() {
+    console.log("My event");
+}
+
+//Assign the event handler to an event
+eventEmitter.on('event1', myEventHandler);
+
+//Fire the 'event1' event:
+eventEmitter.emit('event1');
